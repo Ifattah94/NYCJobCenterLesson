9f6236733676e301ff9d0e8799be40973791442c
Iram Fattah <iramfattah94@gmail.com>
2019-11-06 21:30:32 -0500

    add model for job center

---
+++ NYCJobCentersLesson/JobCenter.swift
@@ -0,0 +1,48 @@
+//
+//  JobCenter.swift
+//  NYCJobCentersLesson
+//
+//  Created by C4Q on 11/6/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct JobCenter: Codable {
+    let borough: String
+    let streetAddress: String
+    let comments: String?
+    let facilityName: String?
+    let city: String
+    let latitude: String?
+    let longitude: String?
+    let zipCode: String
+    let phoneNumber: String
+    enum CodingKeys: String, CodingKey {
+        case borough
+        case streetAddress = "street_address"
+        case comments
+        case facilityName = "facility_name"
+        case city
+        case latitude
+        case longitude
+        case zipCode = "zip_code"
+        case phoneNumber = "phone_number_s"
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        guard let lat = latitude, let long = longitude else {return CLLocationCoordinate2DMake(0.0, 0.0)}
+        guard let latDouble = Double(lat), let longDouble = Double(long) else {return CLLocationCoordinate2DMake(0.0, 0.0)}
+        return CLLocationCoordinate2DMake(latDouble, longDouble)
+        
+    }
+    var primaryPhoneNumber: String {
+        let phoneArr = phoneNumber.components(separatedBy: "/")
+        guard let phoneNumber = phoneArr.first else {return "N/A"}
+        return phoneNumber
+        
+    }
+    
+    
+}
